5272147d498b96e0af6e87ff78f954e51ec92c7c
Mauluk92 <nikolajbogdanorov@gmail.com>
2022-02-18 10:01:01 +0100

    Second Deploy

--- tarot/views.py
+++ tarot/views.py
@@ -2,7 +2,7 @@ from django.template import loader
 from django.http.response import HttpResponse
 
 def homepage(request):
-    template = loader.get_template('tarot/templates/home.html')
+    template = loader.get_template('home.html')
     return HttpResponse(template)
 
 # Create your views here.
