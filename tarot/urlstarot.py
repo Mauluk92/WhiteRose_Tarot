eea157313798d6600e050d32e09ad96fb90ce7ae
Mauluk92 <nikolajbogdanorov@gmail.com>
2022-02-18 10:37:30 +0100

    Fifth Deploy

--- tarot/urlstarot.py
+++ tarot/urlstarot.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import homepage
-from os import path
 
 urlpatterns = [
-     path(r'^media/(?P<path>.*)$','django.views.static.serve', {'document_root': path.join(path.dirname(__file__), 'staticfiles')}),
      path('', homepage),
 ]
